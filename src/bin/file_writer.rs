7a4cf7fb9aaad0cf751648b3a106e02985d60968
Wangshan Lu <wisagan@gmail.com>
2015-01-16 23:15:51 +0800

    Add file_writer.

---
+++ src/bin/file_writer.rs
@@ -0,0 +1,16 @@
+extern crate diffusion;
+use diffusion::Writer;
+use diffusion::FileWriter;
+fn main() {
+    let args = std::os::args();
+    if args.len() < 3 {
+        println!("Usage: {} text_filename dfsn_filename", args[0]);
+        return;
+    }
+    let mut text_file = std::io::BufferedReader::new(std::io::File::open(&Path::new(args[1].as_slice())));
+
+    let mut writer = FileWriter::new(&Path::new(args[2].as_slice())).unwrap();
+    for line in text_file.lines() {
+        writer.try_write(line.unwrap().trim().as_bytes());
+    }
+}
