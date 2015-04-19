37712cbe2f864d0e6b7c28b06456a106fc61d857
Wangshan Lu <wisagan@gmail.com>
2015-04-19 12:00:35 +0800

    Update compiler to after 1.0.0-beta.2

--- src/bin/multicast_writer.rs
+++ src/bin/multicast_writer.rs
@@ -1,22 +1,22 @@
+#![feature(convert)]
 extern crate diffusion;
 use diffusion::Writer;
 use diffusion::MulticastWriter;
 fn main() {
-    let args = std::os::args();
+    let args : Vec<String> = std::env::args().collect();
     if args.len() < 3 {
         println!("Multicast message once every second.");
         println!("Usage: {} multicast_ip multicast_port", args[0]);
         return;
     }
-    let port : u16 = std::str::FromStr::from_str(args[2].as_slice()).unwrap();
+    let port : u16 = std::str::FromStr::from_str(args[2].as_str()).unwrap();
     println!("Connecting to {}:{}", args[1], port);
-    let mut writer = MulticastWriter::new((args[1].as_slice(), port)).unwrap();
+    let mut writer = MulticastWriter::new((args[1].as_str(), port)).unwrap();
     let step = 1i64;
-    let mut count = std::iter::count(0i64, step);
-    for time in count {
-        let message = "This is message No. ".to_string() + time.to_string().as_slice();
-        writer.try_write(message.as_bytes());
+    for time in 0i64..step {
+        let message = "This is message No. ".to_string() + time.to_string().as_str();
+        writer.try_write(message.as_bytes()).unwrap();
         println!("{}", message);
-        std::old_io::timer::sleep(std::time::Duration::seconds(step));
+        std::thread::sleep_ms((step * 1000) as u32);
     }
 }
